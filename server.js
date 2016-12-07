d56a0a300bff660805620e6a9bc2346371182b88
aditiasap <aditiasap@gmail.com>
2016-12-07 18:33:51 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+const Hapi = require('hapi');
+const Blipp = require('blipp');
+const HapiLevel = require('hapi-level');	// micro library to create LevelDB datastore, which expose LevelDB API through
+											// server.plugins['hapi-level'].db and store the reference in variable HapiLevel
+const UserStore = require('./user-store.js');
+const server = new Hapi.Server();
+
+server.connection({
+	port: 1337,
+	host: '127.0.0.1'
+});
+
+server.register([
+	{
+		register: HapiLevel,
+		options: {
+			config: {
+				valueEncoding: 'json'		// set value encoding to json instead of using its default utf8 encoding
+			}
+		}
+	},
+	UserStore,
+	Blipp
+], (err) => {
+	server.start((err) => {
+		console.log(`Server running at ${server.info.uri}`);
+	});
+});
---
+++ user-store.js
@@ -0,0 +1,96 @@
+'use strict';
+const Uuid = require('uuid');	// for userID
+const Boom = require('boom');	// for HTTP-friendly error object
+
+exports.register = function (server, options, next) {
+	let store;
+	
+	// Make dependency on hapi-level plugin, which will be used to connect to levelDB
+	server.dependency('hapi-level', (server, after) => {
+		store = server.plugins['hapi-level'].db;
+		return after();
+	});
+	
+	// getUser as a function to retrieve users from levelDB datastore
+	// separating this from handler to enable us to expose this function, hence can be 
+	// called from anywhere which have server reference through server.plugins.userStore.getUser()
+	const getUser = function (userId, callback) {
+		return store.get(userId, callback);
+	};
+	
+	const createUser = function (userDetails, callback) {
+		const userId = Uuid.v4();
+		const user = {
+			id: userId,
+			details: userDetails
+		};
+		store.put(userId, user, (err) => {
+			callback(err, user);
+		});
+	};
+	
+	server.route([
+		{
+			method: 'GET',
+			path: '/user/{userId}',
+			config: {
+				handler: function (request, reply) {
+					const userId = request.params.userId;
+					getUser(userId, (err, user) => {
+						if(err) {
+							return reply(Boom.notFound(err));
+						}
+						return reply(user);
+					});
+				},
+				description: 'Retrieve a user'
+			}
+		},
+		{
+			method: 'POST',
+			path: '/user',
+			config: {
+				handler: function (request, reply) {
+					const userDetails = request.payload;
+					createUser(userDetails, (err, user) => {
+						if(err) {
+							return reply(Boom.badRequest(err));
+						}
+						return reply(user);
+					});
+				},
+				description: 'Create a user'
+			}
+		}
+	]);
+	
+	server.expose({
+		getUser: getUser,
+		createUser: createUser
+	});
+	
+	return next();
+};
+
+exports.register.attributes = {
+	name: 'userStore'
+};
+
+/* Example server.register's option (registering plugin) to add prefix of /v1 to the routes.
+// Benefit for versioning API
+
+server.register(
+	{
+		register: Hello,
+		options: {}
+	},
+	{
+		routes: {
+			prefix: '/v1'
+		}
+	},
+	(err) => {
+		// Start server
+	}
+);
+*/
