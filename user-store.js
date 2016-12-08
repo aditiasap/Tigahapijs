0d88d6a7a4778359cc24d2f2563c6b00386f19b2
aditiasap <aditiasap@gmail.com>
2016-12-08 18:50:45 +0700

    Add files via upload

--- user-store.js
+++ user-store.js
@@ -29,6 +29,20 @@ exports.register = function (server, options, next) {
 		});
 	};
 	
+	const updateUser = function (userId, userDetails, callback) {
+		const user = {
+			id: userId,
+			details: userDetails
+		};
+		store.put(userId, user, (err) => {
+			callback(err, user);
+		});
+	};
+	
+	const deleteUser = function (userId, callback) {
+		return store.del(userId, callback);
+	};
+	
 	server.route([
 		{
 			method: 'GET',
@@ -61,12 +75,57 @@ exports.register = function (server, options, next) {
 				},
 				description: 'Create a user'
 			}
+		},
+		{
+			method: 'PUT',
+			path: '/user/{userId}',
+			config: {
+				handler: function (request, reply) {
+					const userId = request.params.userId;
+					const userDetails = request.payload;
+					getUser(userId, (err, user) => {
+						if(err) {
+							return reply(Boom.notFound(err));
+						}					
+						updateUser(userId, userDetails, (err, user) => {
+							if(err) {
+								return reply(Boom.badRequest(err));
+							}
+							return reply(user);
+						});
+					});
+				},
+				description: 'Update a user'
+			}
+		},
+		{
+			method: 'DELETE',
+			path: '/user/{userId}',
+			config: {
+				handler: function (request, reply) {
+					const userId = request.params.userId;
+					getUser(userId, (err, user) => {
+						if(err) {
+							return reply(Boom.notFound(err));
+						}
+						deleteUser(userId, (err, user) => {
+							if(err) {
+								return reply(Boom.notFound(err));
+							}
+							return reply({message: `${userId} is deleted`});
+						});
+					});
+				},
+				description: 'Delete a user'
+			}
 		}
 	]);
 	
 	server.expose({
 		getUser: getUser,
-		createUser: createUser
+		createUser: createUser,
+		updateUser: updateUser,
+		deleteUser: deleteUser
 	});
 	
 	return next();
