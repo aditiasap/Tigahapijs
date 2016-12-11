1827b062f5555ab3e682dfc4b676b06f9c53d8c8
aditiasap <aditiasap@gmail.com>
2016-12-11 09:06:22 +0700

    Glue and manifest

---
+++ index.js
@@ -0,0 +1,41 @@
+/* Example of using Glue to start server.
+To start, run : node index.js
+
+Use Glue vers 2.4.0
+*/
+
+const Glue = require('glue');
+const manifest = require('./manifest');
+/*
+const manifest = {
+	server: {},
+	connections: [
+		{
+			port: 1337,
+			host: '127.0.0.1'
+		}
+	],
+	plugins: [
+		{
+			'hapi-level': {
+				path: './temp',
+				config: {
+					valueEncoding: 'json'
+				}
+			}
+		},
+		{
+			'./user-store.js': {}
+		},
+		{
+			'blipp': {}
+		}
+	]
+};
+*/
+
+Glue.compose(manifest, {relativeTo: __dirname}, (err, server) => {
+	server.start((err) => {
+		console.log(`Server running at ${server.info.uri}`);
+	});
+});
---
+++ manifest.js
@@ -0,0 +1,34 @@
+/* Example of dedicating manifest configuration
+Then using rejoice to start server.
+To start, run : ./node_modules/rejoice/bin/rejoice -c manifest.js -p ./
+
+Use rejoice Vers <3. npm install rejoice@"<3"
+*/
+
+const manifest = {
+	server: {},
+	connections: [
+		{
+			port: 1337,
+			host: '127.0.0.1'
+		}
+	],
+	plugins: [
+		{
+			'hapi-level': {
+				path: './temp',
+				config: {
+					valueEncoding: 'json'
+				}
+			}
+		},
+		{
+			'./user-store.js': {}
+		},
+		{
+			'blipp': {}
+		}
+	]
+};
+
+module.exports = manifest;
